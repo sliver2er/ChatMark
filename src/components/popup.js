70b13f7a5e47cdae6d61c9cfdf9ebf308b745479
sliver2er <shpaul2001@snu.ac.kr>
2025-11-23 05:38:38 +0900

    MVP init

--- src/components/popup.js
+++
@@ -1,5 +0,0 @@
-  window.addEventListener("message", (event) => {
-    if (event.data.type === "SIDEQUEST_SELECTED_TEXT") {
-      document.body.innerHTML += `<p>Selected: ${event.data.text}</p>`;
-    }
-  });
