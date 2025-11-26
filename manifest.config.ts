85f051911b749ceb66d316ac2db6f95ecad8bf04
sliver2er <shpaul2001@snu.ac.kr>
2025-11-27 00:49:04 +0900

    restart

---
+++ manifest.config.ts
@@ -0,0 +1,24 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export default defineManifest({
+  manifest_version: 3,
+  name: "ChatMark",
+  version: "1.0.0",
+  action: {
+    default_popup: "panel/index.html"
+  },
+  permissions: ["storage", "activeTab", "scripting"],
+  background: {
+    service_worker: "background/index.ts",
+    type: "module"
+  },
+  content_scripts: [
+    {
+      matches: [
+        "https://chat.openai.com/*",
+        "https://chatgpt.com/*"
+      ],
+      js: ["scripts/index.ts"]
+    }
+  ]
+});
